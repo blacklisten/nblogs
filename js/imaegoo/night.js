6284d3368527aa96b9c779131c5999d1ee383de7
blacklisten <blacklisten@users.noreply.github.com>
2021-04-09 08:54:29 +0000

    deploy: 42ff420962d37727747257cfbda3002e760e2eb8

---
+++ js/imaegoo/night.js
@@ -0,0 +1,58 @@
+(function() {
+  /**
+   * Icarus 夜间模式 by iMaeGoo
+   * https://www.imaegoo.com/
+   */ 
+  var isNight = localStorage.getItem('night');
+  var nightNav;
+
+  function applyNight(value) {
+      if (value.toString() === 'true') {
+        document.body.classList.remove('white');
+          document.body.classList.add('night');
+          // if (document.querySelector('.snow-container')) {
+          //     document.querySelector('.snow-container').setAttribute("style", "z-index: 0")
+          // }
+      } else {
+          document.body.classList.remove('night');
+          document.body.classList.add('white');
+          //  if (document.querySelector('.snow-container')) {
+          //     document.querySelector('.snow-container').setAttribute("style", "z-index: 10001")
+          //  }
+     }
+     searchDisqusThread(value.toString())
+  }
+
+  function searchDisqusThread(value) {
+      if (document.querySelector('#disqus_thread')) {
+          if (value === 'true') {
+            document.querySelector('#disqus_thread').parentNode.parentNode.setAttribute("style", "background: #b5b5b5b8")
+          } else {
+            document.querySelector('#disqus_thread').parentNode.parentNode.setAttribute("style", "background: #FFFFFF")
+          }
+      } else {
+          var clearTime = setTimeout(() => {
+            searchDisqusThread(value)
+            clearTimeout(clearTime)  
+          })
+      }
+  }
+
+  function findNightNav() {
+      nightNav = document.getElementById('night-nav');
+      if (!nightNav) {
+          setTimeout(findNightNav, 100);
+      } else {
+          nightNav.addEventListener('click', switchNight);
+      }
+  }
+
+  function switchNight() {
+      isNight = isNight ? isNight.toString() !== 'true' : true;
+      applyNight(isNight);
+      localStorage.setItem('night', isNight);
+  }
+
+  findNightNav();
+  isNight && applyNight(isNight);
+}());
